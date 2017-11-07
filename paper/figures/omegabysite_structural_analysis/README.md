d073812f343be50382e4b5c924873fcde363941e
Hugh Haddox <haddox@uw.edu>
2017-11-07 10:51:46 -0800

    Add analysis of our results compared to the sites identified by the Ward group to be involved in conformational dynamics

--- paper/figures/omegabysite_structural_analysis/README.md
+++ paper/figures/omegabysite_structural_analysis/README.md
@@ -1,1 +1,7 @@
-This folder contains its own script for generating the snapshots of Env's structure colored by omega-by-site value, as made in `PyMOL`. This script is called `analysis_code.py` and can be run in the `PyMOL` terminal in this directory using the command: `run analysis_code.py`. It will then generate PNG files of the images shown in this figure. I copied and pasted these images into the PowerPoint `omegabysite_structural_analysis.pptx` to make the complete figure.
+# Coloring Env's structure by omega-by-site values
+
+This folder contains two scripts for generating the snapshots of Env's structure colored by omega-by-site value, as made in `PyMOL`.
+
+* *analysis_code_omega_by_site.py*: generates PNG files of the images of the entire Env structure (5FYL) colored by omega-by-site values, as shown in this figure. I copied and pasted these images into the PowerPoint `omegabysite_structural_analysis.pptx` to make the complete figure.
+
+* *analysis_code_conformational_dynamics_network.py*: generates a PyMOL session, which I manually tweaked to obtain an image of Env (5VN3) zoomed in to the region around alpha-helix 0 and the network of hydrophobic amino acids identified by Ozorowski et al., 2017, Nature to be involved in the conformational change that occurs upon CD4 binding.
--- paper/figures/omegabysite_structural_analysis/analysis_code_conformational_dynamics_network.py
+++ paper/figures/omegabysite_structural_analysis/analysis_code_conformational_dynamics_network.py
@@ -2,7 +2,7 @@
 This script is for analyzing omega-by-site values in PyMOL
 
 It is intended to be run in the PyMOL terminal as:
-    run analysis_code.py
+    run analysis_code_conformational_dynamics_network.py
 
 Hugh Haddox, November-2-2017
 """
@@ -11,43 +11,40 @@ Hugh Haddox, November-2-2017
 import pymol
 from pymol import cmd
 
+# Fetch structure
+structure = '5vn3'
+cmd.reinitialize()
+cmd.delete('all')
+cmd.fetch(structure) #, type='pdb1')
+
+# Remove non-Env chains
+# gp41 = chain A
+# gp120 = chain G
+cmd.remove ('c;C,H,I,J,K,L,M,N')
+#cmd.symexp(structure, structure, structure, 3) # generate symmetry partners based on crystal structure
+
+# Tweak initial display and color of Env monomers
+cmd.hide('everything')
+cmd.bg_color('white')
+cmd.show('cartoon')
+cmd.color('grey90')
+#cmd.color('grey20', structure)
+cmd.set('cartoon_transparency', '0.5')
+cmd.set('cartoon_transparency', '0', structure)
+#cmd.hide('hetatm')
+
+# Identify unique sites in structure
+sites_in_structure = []
+cmd.iterate("(name ca)","sites_in_structure.append(resi)")
+unique_sites_in_structure = []
+for site in sites_in_structure:
+	if site not in unique_sites_in_structure:
+		unique_sites_in_structure.append(site)
+
+# Get omega-by-site values and determine which sites evolve significantly
+# faster or slower than expected
 homologs = ['BG505', 'BF520']
 for h in homologs:
-
-	# Fetch structure
-	structure = '5fyl'
-	cmd.reinitialize()
-	cmd.delete('all')
-	cmd.fetch(structure) #, type='pdb1')
-
-	# Remove non-Env chains
-	# gp41 = chain B
-	# gp120 = chain G
-	# 35O22 = chains D and E
-	# PGT122 = chains H and L
-	cmd.remove ('c;D,E,H,L,U,V')
-	cmd.symexp(structure, structure, structure, 3) # generate symmetry partners based on crystal structure
-
-	# Tweak initial display and color of Env monomers
-	cmd.hide('everything')
-	cmd.bg_color('white')
-	cmd.show('cartoon')
-	cmd.color('grey90')
-	#cmd.color('grey20', structure)
-	cmd.set('cartoon_transparency', '0.5')
-	cmd.set('cartoon_transparency', '0', structure)
-	#cmd.hide('hetatm')
-
-	# Identify unique sites in structure
-	sites_in_structure = []
-	cmd.iterate("(name ca)","sites_in_structure.append(resi)")
-	unique_sites_in_structure = []
-	for site in sites_in_structure:
-		if site not in unique_sites_in_structure:
-			unique_sites_in_structure.append(site)
-
-	# Get omega-by-site values and determine which sites evolve significantly
-	# faster or slower than expected
 	omega_d = {'BG505':{}, 'BF520':{}}
 	sites_sig_fast = {'BG505':[], 'BF520':[]}
 	sites_sig_slow = {'BG505':[], 'BF520':[]}
@@ -61,66 +58,56 @@ for h in homologs:
 				sites_sig_slow[Env].append(site)
 			if float(Q) < Q_cutoff and float(omega) > 1.0:
 				sites_sig_fast[Env].append(site)
-	min_omega = min(omega_d['BG505'].values() + omega_d['BF520'].values())
-	max_omega = max(omega_d['BG505'].values() + omega_d['BF520'].values())
-	print "\nThe minimum and maximum omega-by-site values are:"
-	print "min: {0}".format(min_omega)
-	print "max: {0}".format(max_omega)
+min_omega = min(omega_d['BG505'].values() + omega_d['BF520'].values())
+max_omega = max(omega_d['BG505'].values() + omega_d['BF520'].values())
+print "\nThe minimum and maximum omega-by-site values are:"
+print "min: {0}".format(min_omega)
+print "max: {0}".format(max_omega)
 
-	# Color residues by omega-by-site log10P values
+# Color residues by the average omega-by-site log10P values
+BG505_sites = sorted(omega_d['BG505'].keys())
+BF520_sites = sorted(omega_d['BF520'].keys())
+assert BG505_sites == BF520_sites
+sites_with_data = BG505_sites
+sites_not_in_structure = []
+for site in sites_with_data:
+    avg_log10P_dir = (omega_d['BG505'][site] + omega_d['BF520'][site])/2.0
+    cmd.alter("{0} and resi {1}".format(structure, site), "b = {0}".format(avg_log10P_dir))
+    if site not in unique_sites_in_structure:
+    	sites_not_in_structure.append(site)
+cmd.spectrum('b', 'red_grey_blue', structure, minimum = min_omega, maximum = max_omega)
+print ("\nSites with data not in structure: {0}".format(', '.join(sites_not_in_structure)))
 
-	BG505_sites = sorted(omega_d['BG505'].keys())
-	BF520_sites = sorted(omega_d['BF520'].keys())
-	assert BG505_sites == BF520_sites
-	sites_with_data = BG505_sites
-	sites_not_in_structure = []
-	for site in sites_with_data:
-	    cmd.alter("{0} and resi {1}".format(structure, site), "b = {0}".format(omega_d[h][site]))
-	    if site not in unique_sites_in_structure:
-	    	sites_not_in_structure.append(site)
-	cmd.spectrum('b', 'red_grey_blue', structure, minimum = min_omega, maximum = max_omega)
-	print ("\nSites with data not in structure: {0}".format(', '.join(sites_not_in_structure)))
+# Color residues lacking data white
+sites_lacking_data = [site for site in unique_sites_in_structure if site not in sites_with_data]
+print ("\nThe following sites in the structure, but lacking data will be colored white: {0}".format(', '.join(sites_lacking_data)))
+cmd.color('white', '{0} and resi {1}'.format(structure, '+'.join(sites_lacking_data)))
 
-	# Color residues lacking data white
-	sites_lacking_data = [site for site in unique_sites_in_structure if site not in sites_with_data]
-	print ("\nThe following sites in the structure, but lacking data will be colored white: {0}".format(', '.join(sites_lacking_data)))
-	cmd.color('white', '{0} and resi {1}'.format(structure, '+'.join(sites_lacking_data)))
+# Define groups of significant sites found in both homologs
+sites_sig_slow_both_homologs = [site for site in sites_with_data if site in sites_sig_slow['BG505'] and site in sites_sig_slow['BF520']]
+cmd.select('sites_sig_slow', '{0} and resi {1}'.format(structure, '+'.join(sites_sig_slow_both_homologs)))
+print ("\nUsing a Q-value cutoff of {0}, there are {1} sites that evolve slower than expected in both homologs:".format(Q_cutoff, len(sites_sig_slow_both_homologs)))
+print (', '.join(map(str, sorted(map(int, sites_sig_slow_both_homologs)))))
 
-	# Show significant sites as spheres
+
+"""
+for h in homologs:
+	print ("\nAnalyzing {0}".format(h))
 	print ("\nUsing a Q-value cutoff of {0}".format(Q_cutoff))
 	print ("\n{0} sites that evolve slower than expected with significant Q values: {1}".format(len(sites_sig_slow[h]), ', '.join(sites_sig_slow[h])))
 	print ("\n{0} sites that evolve faster than expected with significant Q values: {1}".format(len(sites_sig_fast[h]), ', '.join(sites_sig_fast[h])))
 	cmd.select('sites_sig_slow', '{0} and resi {1}'.format(structure, '+'.join(sites_sig_slow[h])))
 	cmd.select('sites_sig_fast', '{0} and resi {1}'.format(structure, '+'.join(sites_sig_fast[h])))
-	cmd.show('spheres', 'sites_sig_fast')
-	cmd.show('spheres', 'sites_sig_slow')
-
-	# Take a pictures of Env rotated 120 degrees relative to one another
-	cmd.set_view ("""\
-	     0.413237274,    0.018612767,   -0.910427451,\
-	     0.910516560,    0.006436472,    0.413409144,\
-	     0.013554142,   -0.999800861,   -0.014289021,\
-	    -0.000450239,   -0.000266090, -364.028167725,\
-	    64.596366882,   39.767127991,   -5.709826946,\
-	   224.197448730,  503.894958496,  -20.000000000""")
-	take_pictures = True
-	if take_pictures:
-		cmd.bg_color('white')
-		cmd.png('{0}_face1.png'.format(h), width=1000, dpi=1000, ray=1)
-		cmd.rotate('y', '120')
-		cmd.png('{0}_face2.png'.format(h), width=1000, dpi=1000, ray=1)
-
-
-
-
-
-
-
-
+"""
 
 
 
 # Annotations of structural features
+# a0 helix
+cmd.select('a0 helix', structure +' and resi 63-72')
+# network of hydrophobic residues from Ozorowski, 2017, Nature
+cmd.select('hydrophobic network', structure +' and resi 435+427+111+112+69+72+564')
+
 # gp120 and gp41
 cmd.select('gp120', structure + ' and resi 31-511')
 cmd.select('gp41', structure +' and resi 512-664')
@@ -156,18 +143,3 @@ cmd.select('V1_V2_epitope', structure + ' and resi 156+158+160+162+163+164+165+1
 cmd.select('gp120_gp41_epitope', structure + ' and resi 44+45+46+58+80+82+83+84+85+87+88+90+91+92+93+94+229+230+231+232+234+237+238+240+241+245+246+262+276+277+278+352+448+462+512+513+514+515+516+517+518+519+520+521+522+527+529+542+543+549+550+554+592+611+613+615+616+617+618+620+621+624+625+627+629+630+632+633+634+636+637+638+640+641+643+644+647')
 cmd.select('fusion_peptide', structure + ' and resi 512-527')
 cmd.select(None)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
