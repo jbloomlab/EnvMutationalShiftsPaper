d10e7efbd8704fbde69e2e1fe62c968df8b119f9
jbloom <jbloom@fredhutch.org>
2018-02-17 06:04:50 -0800

    add WT to plots for shifted sites, re-run notebook
    We added the wildtype amino acid above the Env for
    each site (this is panel Figure 6C).
    
    Also, the notebook was cleanly run from the top
    and paper was modified to indicate version
    2.2.4 of `dms_tools2` was used.

--- make_supp_figs.bash
+++ make_supp_figs.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# make supplementary files from analysis code
+
 jupyter nbconvert analysis_code/analysis_notebook.ipynb && mv analysis_code/analysis_notebook.html paper/figures/
 
 zip -r --exclude=analysis_code/run.sbatch --exclude=analysis_code/results/FASTQ_files/* --exclude=analysis_code/.* analysis_code.zip analysis_code && mv analysis_code.zip paper/figures/
